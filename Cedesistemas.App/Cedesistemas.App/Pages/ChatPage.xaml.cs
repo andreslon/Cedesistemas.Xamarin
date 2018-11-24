5ac4d7300f254744656651a524a023bf0df6045d
Andrés Londoño <andreslon@outlook.com>
2018-11-23 23:05:52 -0500

    Primera clase

---
+++ Cedesistemas.App/Cedesistemas.App/Pages/ChatPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.TextToSpeech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cedesistemas.App.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChatPage : ContentPage
+	{
+		public ChatPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async private void EnviarBtn_Clicked(object sender, EventArgs e)
+        {
+            ChatNombreTxt.Text = NombreTxt.Text + " dijo:";
+            ChatMensajeTxt.Text = MensajeTxt.Text;
+
+            NombreTxt.Text = string.Empty;
+            MensajeTxt.Text = string.Empty;
+
+            await CrossTextToSpeech.Current.Speak(ChatMensajeTxt.Text);
+        }
+    }
+}
---
+++ Cedesistemas.App/Cedesistemas.App/Pages/HomePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cedesistemas.App.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage ()
+        {
+            InitializeComponent();
+            //this.Children.Add(new ChatPage());
+            //this.Children.Add(new FormPage());
+        }
+    }
+}
